6d10a73be03993a9847e41444835769143762cc9
Denise Judd <djudd0720@yahoo.com>
2018-04-17 12:46:34 -0500

    NameAddrJavaProgram

---
+++ NameAddressforGitHub.java
@@ -0,0 +1,38 @@
+package javaClass;
+
+public class NameAddressforGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Test message");
+		   
+		   //Declaring the variable
+		   String name;
+		   String streetaddr;
+		   String city;
+		   String state;
+		   String zipcode;
+		   
+		   //Define the variable
+		   name="Denise Judd";
+		   streetaddr = "1234 Claimacademy Drive";
+		   city = "St. Louis";
+		   state = "Missouri";
+		   zipcode = "63101";
+				   
+		   	   
+		   //Declare and Define variable
+		   int age = 30;
+		   
+		 	 
+		   
+		   System.out.println(name);
+		   System.out.println("Hi " + name + ", welcome back! Below is your information:");
+		   System.out.println("Age: " + age);
+		   System.out.println("Street: " + streetaddr);
+		   System.out.println("City: "+ city);
+		   System.out.println("State: "+ state);
+		   System.out.println("Zip: " + zipcode);
+
+	}
+
+}
